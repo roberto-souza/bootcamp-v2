844937f653912db59a0ca6f2e412d6bda1dd8ed3
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2020-05-15 20:53:20 -0300

    feat: 🎸 conceitos-dev > backend Aplicação funcional

--- conceitos-dev/backend/src/index.js
+++ conceitos-dev/backend/src/index.js
@@ -1,9 +1,61 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send({ message: "hello world" });
+app.use(express.json());
+
+const projects = [];
+
+app.get("/projects", (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
+});
+
+app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found" });
+  }
+
+  const project = { id, title, owner };
+
+  projects[projectIndex] = project;
+
+  return response.json(projects);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found" });
+  }
+
+  projects.slice(projectIndex, 1);
+
+  return response.sendStatus(204);
 });
 
 app.listen(3333, () => {
