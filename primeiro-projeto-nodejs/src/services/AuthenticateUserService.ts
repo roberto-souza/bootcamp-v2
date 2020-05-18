992da72b7f0866aff2bd9f7458b12a17704fa6dc
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2020-05-18 17:50:46 -0300

    Auth com JWT

---
+++ primeiro-projeto-nodejs/src/services/AuthenticateUserService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface RequestDTO {
+  email: string;
+  password: string;
+}
+
+interface ResponseDTO {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: RequestDTO): Promise<ResponseDTO> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, '76a7bda2f124bb43634e10bb7084c2eb', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
