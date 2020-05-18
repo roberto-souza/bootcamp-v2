87c22cfb02b87bbd20111aa87c4a6fecd83db6d7
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2020-05-18 16:59:21 -0300

    Ajustando relacionamentos

--- primeiro-projeto-nodejs/src/database/migrations/1589678885292-CreateAppointments.ts
+++ primeiro-projeto-nodejs/src/database/migrations/1589678885292-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1589678885292
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
