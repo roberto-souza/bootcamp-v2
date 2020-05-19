9c234e7ae2aca1172cc2f08b153b0cc46f322eaa
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2020-05-19 09:24:13 -0300

    Configuração para upload de arquivos

--- primeiro-projeto-nodejs/src/routes/users.routes.ts
+++ primeiro-projeto-nodejs/src/routes/users.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -22,8 +26,13 @@ usersRouter.post('/', async (req, res) => {
   }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (req, res) => {
-  return res.json({ ok: true });
-});
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    return res.json({ ok: true });
+  },
+);
 
 export default usersRouter;
