0ca1b535add6085d19c4e062a653b465e1088155
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2020-06-05 22:21:19 -0300

    Injeção de dependências

--- primeiro-projeto-nodejs/src/modules/users/infra/http/routes/sessions.routes.ts
+++ primeiro-projeto-nodejs/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express';
+import { container } from 'tsyringe';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (req, res) => {
   const { email, password } = req.body;
 
-  const usersRepository = new UsersRepository();
-  const authenticateUser = new AuthenticateUserService(usersRepository);
+  const authenticateUser = container.resolve(AuthenticateUserService);
 
   const { user, token } = await authenticateUser.execute({ email, password });
 
