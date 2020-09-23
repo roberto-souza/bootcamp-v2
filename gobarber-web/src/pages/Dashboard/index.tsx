f9b20df7eb279482af7a3b1f1a327d6ac16090a2
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2020-09-23 09:50:59 -0300

    lista de agendamentos na dashboard

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 import { useAuth } from '../../hooks';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   return (
     <Container>
@@ -28,6 +41,88 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/22962926?s=460&u=d82b89e81f3489b356a88adcb2dc68462d968857&v=4"
+                alt="Roberto Souza"
+              />
+
+              <strong>Roberto Souza</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/22962926?s=460&u=d82b89e81f3489b356a88adcb2dc68462d968857&v=4"
+                  alt="Roberto Souza"
+                />
+
+                <strong>Roberto Souza</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/22962926?s=460&u=d82b89e81f3489b356a88adcb2dc68462d968857&v=4"
+                  alt="Roberto Souza"
+                />
+
+                <strong>Roberto Souza</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/22962926?s=460&u=d82b89e81f3489b356a88adcb2dc68462d968857&v=4"
+                  alt="Roberto Souza"
+                />
+
+                <strong>Roberto Souza</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
