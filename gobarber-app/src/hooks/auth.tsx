d6fd3167784c2c4e0b708fb4b76fe91d7e62bade
Roberto Pedroso de Souza <robertopedrosodesouza@gmail.com>
2020-09-24 11:20:13 -0300

    Rotas + Header dashboard

--- gobarber-app/src/hooks/auth.tsx
+++ gobarber-app/src/hooks/auth.tsx
@@ -3,6 +3,12 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatarUrl: string;
+}
 interface AuthState {
   token: string;
   user: object;
@@ -15,7 +21,7 @@ interface SignInCredentials {
 
 interface IAuthContext {
   loading: boolean;
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
